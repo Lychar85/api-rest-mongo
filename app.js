9c24b4c2e0fb90e05794fbefd569bb3fa7a96097
Your Name <you@example.com>
2021-02-25 15:36:29 +0100

    .

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ port = 1900
 
 
 //mongoose
-mongoose.connect('mongodb://localhost:27017/monblog')
+mongoose.connect('mongodb://localhost:27017/monblog',{useNewUrlParser: true,useUnifiedTopology: true})
+console.log("status mongo:",mongoose.connection.readyState);
+
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 
 //route
 const {getHomePage} = require('./routes/index')
+const authRoute = require('./routes/auth')
 
 
-
-
-//index
+app.use('/api/auth', authRoute)
 app.get('/', getHomePage)
 
 app.listen(port, () => console.log(`server connect to port : ${port}`))
